de091d881b82f30970ff41f76ad31ce664225db7
Yubin Lim <yubin.lim@smail.astate.edu>
2021-04-29 19:47:27 -0500

    Here is the change!

---
+++ main.js
@@ -0,0 +1,17 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+
+/* Looping through images */
+
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  thumbBar.appendChild(newImage);
+
+
+
+
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
